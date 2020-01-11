70e7ec70948f7104208e73119bd7d5ea40ec78e7
Aizat Faiz <aizat.faiz@gmail.com>
2020-01-11 11:13:05 +0800

    fix: Middot for only one children

--- build/Middot.js
+++ build/Middot.js
@@ -5,10 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importDefault(require("react"));
 function Middot(props) {
-    var length = props.children.length;
+    var propsChildren = react_1.default.Children.toArray(props.children);
+    var length = propsChildren.length;
     var children = [];
     for (var i = 0; i < length; i++) {
-        var child = props.children[i];
+        var child = propsChildren[i];
         children.push(child);
         if (i < length - 1) {
             children.push(react_1.default.createElement("span", { key: i }, " \u00B7 "));
--- src/Middot.tsx
+++ src/Middot.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 export function Middot(props): JSX.Element {
-  const length = props.children.length;
+  const propsChildren = React.Children.toArray(props.children);
+  const length = propsChildren.length;
 
   const children = [];
   for (let i = 0; i < length; i++) {
-    const child = props.children[i];
+    const child = propsChildren[i];
     children.push(child);
 
     if (i < length - 1) {
